09e34c0962fea6026a633124d90fa43b8e1112f7
rogersanick <rogers.a.nick@gmail.com>
2019-05-10 11:53:54 +1000

    Start upgrade to 4.0 proper.

--- kotlin-source/src/main/kotlin/net/corda/accord/contract/PromissoryNoteContract.kt
+++ kotlin-source/src/main/kotlin/net/corda/accord/contract/PromissoryNoteContract.kt
@@ -4,7 +4,7 @@ import net.corda.core.contracts.*
 import net.corda.core.transactions.LedgerTransaction
 import net.corda.finance.contracts.asset.Cash
 import net.corda.accord.state.PromissoryNoteState
-import net.corda.finance.utils.sumCash
+import net.corda.finance.contracts.utils.sumCash
 import java.security.PublicKey
 
 /**
