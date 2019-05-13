f15bbea4f950ec3e2563c7220e53126729327f0b
Jerome Simeon <jeromesimeon@me.com>
2019-05-13 10:42:03 -0400

    (fix) Code generate org.* CTO classes in kotlin
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- kotlin-source/src/main/kotlin/org/accordproject/base/Asset.java
+++
@@ -1,9 +0,0 @@
-// this code is generated and should not be modified
-package org.accordproject.base;
-
-import net.corda.core.serialization.CordaSerializable;
-import org.hyperledger.composer.system.Resource;
-
-@CordaSerializable
-public abstract class Asset extends Resource {
-}
